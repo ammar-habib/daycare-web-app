5862c753448425db47226dcbaa8e236fe37fe7dd
DESKTOP-BHNO2OR\ammar habib <ammarhabib11@yahoo.com>
2018-02-13 00:33:45 +0500

    final

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,16 @@
 
 $(document).ready(function () {
-
-	$(".responsive-calendar").responsiveCalendar({
+	$("#show-notification").click(function(){
+			$(".notification-section").css("display", "block");
+			$("body").css("background-color","rgba(0,0,0,0.5)");
+		});
+		$("#notification-close-btn").click(function(){
+			$("body").css("background-color","rgba(255,255,255,0.5)");
+			$(".notification-section").css("display", "none");
+			
+		});
+		
+	$("#booking-daycare-spot-calendar").responsiveCalendar({
 		time: '2018-03',
 		events: {
 			"2018-03-13": {"number": 5, "url": "http://localhost/daycare-app/booking-spot-popup.html"},
@@ -11,11 +20,23 @@ $(document).ready(function () {
 			"2018-02-16":{"number": 1},
 			"2018-06-17": {}}
 		});
-	
+	$("#sell-daycare-spot-calendar").responsiveCalendar({
+		time: '2018-03',
+		events: {
+			"2018-03-13": {"number": 5, "url": "http://localhost/daycare-app/sell-daycare-spot-popup.html"},
+			"2018-03-14": {"number": 1, "url": "http://localhost/daycare-app/sell-daycare-spot-popup.html"}, 
+			"2018-03-15":{"number": 1}, 
+			"2018-03-16":{"number": 1},
+			"2018-02-16":{"number": 1},
+			"2018-06-17": {}}
+		});
+   $(".datetimepicker").datepicker();
 });
 
+
+
 function openNav() {
-	document.getElementById("mySidenav").style.width = "250px";
+	document.getElementById("mySidenav").style.width = "230px";
 }
 function closeNav() {
 	document.getElementById("mySidenav").style.width = "0";
@@ -23,7 +44,9 @@ function closeNav() {
 
 
 
+$( document ).ready(function() {
 
+});
 
 
 
